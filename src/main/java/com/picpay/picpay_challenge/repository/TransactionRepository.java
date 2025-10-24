2e3a8f1ee0cb15021781aa5293599f2d644762bb
RiquelSantos <riquesantosoliveira@gmail.com>
2025-10-24 14:19:44 -0300

    feat: Adiciona camada de repository

---
+++ src/main/java/com/picpay/picpay_challenge/repository/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.picpay.picpay_challenge.repository;
+
+import com.picpay.picpay_challenge.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
