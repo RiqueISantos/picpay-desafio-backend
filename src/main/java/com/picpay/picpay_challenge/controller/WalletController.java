0a6f3b6191a8981b2b5ebbdddacd808714dfc98c
RiquelSantos <riquesantosoliveira@gmail.com>
2025-11-04 15:44:47 -0300

    refactor: Remove import desnecessario

--- src/main/java/com/picpay/picpay_challenge/controller/WalletController.java
+++ src/main/java/com/picpay/picpay_challenge/controller/WalletController.java
@@ -10,8 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/wallet")
